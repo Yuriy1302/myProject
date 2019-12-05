7a1f9159f9473072e0e78aa162080c1a9aa2782b
Yuriy13202 <ascendo@list.ru>
2019-12-05 19:34:48 +0700

    Adding modals

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,37 @@
 import '../scss/style.scss';
 
-console.log('Works!');
+/* let myWindow = document.documentElement.clientWidth;
+alert(myWindow); */
+
+/* let buttonFeadback = document.querySelector('.button-chart');
+let buttonPhone = document.querySelector('.button-phone');
+
+let modalFeadback = document.querySelector('.modal-container-feadback-1440');
+let modalPhone = document.querySelector('.modal-container-call-1440');
+
+let modal = document.querySelectorAll('.modal');
+
+buttonFeadback.addEventListener('click', function() {
+    modalFeadback.classList.remove('hidden-modal');
+});
+
+buttonPhone.addEventListener('click', function() {
+    modalPhone.classList.remove('hidden-modal');
+});
+
+let buttonClose = document.querySelectorAll('.button-close'); */
+
+/* for (let i = 0; i < buttonClose.length; i++) {
+    buttonClose[i].addEventListener('click', function() {
+        modal[i].classList.toggle('hidden-modal');
+    });
+} */ /* Рабочий вариант */
+
+/* for (let i = 0; i < buttonClose.length; i++) {
+    buttonClose[i].addEventListener('click', function() {
+        
+        modal[i].classList.toggle('hidden-modal');
+    });
+}
+ */
+
